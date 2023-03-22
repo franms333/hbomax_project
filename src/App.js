ce6bd40e30aa9c86466659e5e5c5df848a294c00
franms333 <franms099@gmail.com>
2023-03-22 15:47:47 -0300

    fifth commit: added form in LoginForm.js component and then added that component inside Login.js component. Also implemented login and home routes using react-router. Last, implementedauthentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
+import Login from './components/SignIn/Login';
 import RecentlyAddedMovies from './components/trending/movies/RecentlyAddedMovies';
 import MainSeries from './components/trending/series/MainSeries';
 import OriginalSeries from './components/trending/series/OriginalSeries';
@@ -8,6 +9,8 @@ import SeriesByCategory from './components/trending/series/SeriesByCategory';
 
 import LoadingSpinner from './components/UI/LoadingSpinner';
 import useLoadingStore from './store/loading-store';
+
+import {Redirect, Route, Switch} from 'react-router-dom';
 
 function App() {
   const [finishedTrendingShows, 
@@ -22,37 +25,49 @@ function App() {
   return (
     <>
         <Header />
-        {/* <div className='card-container'>
-          <Login />
-        </div> */}
-        <LoadingSpinner />
-        <main>
-          <MainSeries />
-          <div className='topGradient' />
-          <SeriesByCategory 
-          type='tv'
-          category='trending'
-          title='Trending TV Shows'
-          marginTop='overlayMarginTop'
-          size='_w'
-          imagePerSlide='6'
-          onFinish={finishedTrendingShows}/>
-          <OriginalSeries/>
-          <SeriesByCategory 
-          type='anime'
-          category='trending'
-          title='Anime'
-          size='_ca'
-          imagePerSlide='9'
-          onFinish={finishedAnimeShows}/>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/login' />
+          </Route>
+
+          <Route path='/login'>
+            <div className='card-container'>
+              <Login />
+            </div>
+          </Route>
+
+          <Route path='/home'>
+            <LoadingSpinner />
+            <main>
+              <MainSeries />
+              <div className='topGradient' />
+              <SeriesByCategory 
+              type='tv'
+              category='trending'
+              title='Trending TV Shows'
+              marginTop='overlayMarginTop'
+              size='_w'
+              imagePerSlide='6'
+              onFinish={finishedTrendingShows}/>
+              <OriginalSeries/>
+              <SeriesByCategory 
+              type='anime'
+              category='trending'
+              title='Anime'
+              size='_ca'
+              imagePerSlide='9'
+              onFinish={finishedAnimeShows}/>
+              
+              <RecentlyAddedMovies 
+              type='movie'
+              category='trending'
+              onFinish={finishedRecentMovies}/>
+            </main>
+            
+            <Footer />
+          </Route>
           
-          <RecentlyAddedMovies 
-          type='movie'
-          category='trending'
-          onFinish={finishedRecentMovies}/>
-        </main>
-        
-        <Footer />
+        </Switch>
       </>
   );
 }
